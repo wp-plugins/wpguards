72066ea7de3a54e3f112844be421940596921ff4
Bartko.co.uk <Bartko.co.uk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-12 10:48:39 +0000

    version fix
    git-svn-id: https://plugins.svn.wordpress.org/wpguards/tags/1.2.4@873954 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpguards.php
+++ wpguards.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPGuards
  * Plugin URI: http://wpguards.com
  * Description: WordPress Support and Maintenance on Autopilot
- * Version: 1.2.0
+ * Version: 1.2.4
  * Author: WordPressGeeks
  * Author URI: http://wordpressgeeks.co.uk
  * License: GPL2
